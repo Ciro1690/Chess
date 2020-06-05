3f82149ea77a80eca2b700983a5caeadb9dbe4b9
Ciro <Ciro16@gmail.com>
2020-06-05 12:12:55 -0600

    added move_piece

--- Board.rb
+++ Board.rb
@@ -2,7 +2,34 @@ require_relative 'Piece'
 
 class Board
 
-    def move_piece(start_pos, end_pos)
+    attr_accessor :board
 
+    def initialize
+        row,col,default_value = 8,8,Piece.new
+        @board = Array.new (row) {Array.new(col,default_value)}
+    end
+
+    def [](pos)
+        x,y = pos
+        board[x][y]
+    end
+
+    def []=(pos,value)
+        x,y = pos
+        board[x][y] = value
+    end
+
+    def move_piece(start_pos, end_pos)
+        x,y = start_pos
+        x2,y2 = end_pos
+
+        if board[x][y].pos.nil?
+            raise "there's no piece at this starting position"
+        elsif !board[x2][y2].valid_move?
+            raise "the piece cannot move to this ending position"
+        end
     end
 end
+
+b = Board.new
+p b.move_piece([0,0],[6,6])
--- Piece.rb
+++ Piece.rb
@@ -1,24 +1,38 @@
 class Piece
 
-    def initialize(color,board,pos)
+    attr_reader :color, :board, :pos
+
+    def initialize
         @color = color
         @board = board
         @pos = pos
     end
+
+    def valid_move
+
+    end
 end
 
 class NullPiece < Piece
-
+    def initialize
+        super(color,board,pos)
+    end
 end
 
 class SlidingPiece < Piece
-
+    def initialize
+        super(color,board,pos)
+    end
 end
 
 class SteppingPiece < Piece
-
+    def initialize
+        super(color,board,pos)
+    end
 end
 
 class Pawn < Piece
-
+    def initialize
+        super(color,board,pos)
+    end
 end
