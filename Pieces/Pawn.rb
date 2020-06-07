337d538162ee7dbfa0fa1af43bec8580506cf7b5
Ciro <Ciro16@gmail.com>
2020-06-07 12:36:41 -0600

    updated pawn.rb

--- Pieces/Pawn.rb
+++ Pieces/Pawn.rb
@@ -4,19 +4,15 @@ class Pawn < Piece
         :P.colorize(color)
     end
     
-    def move_dirs
-        if forward_dir == 1
-            
-        elsif forward_dir == -1
-            
-        end
+    def moves
+        forward_steps + side_attacks
     end
 
     private
     def at_start_row?
         if self.pos[0] == 1 && self.color == :black
             return true
-        elsif self.pos[0] == 7 && self.color == :white
+        elsif self.pos[0] == 6 && self.color == :white
             return true
         else
             return false
@@ -32,10 +28,30 @@ class Pawn < Piece
     end
 
     def forward_steps
+        i,j = pos
+        one_step = [i + forward_dir, j]
+        return [] unless board.valid_moves(one_step) && board.empty?(one_step)
 
+        steps = [one_step]
+        two_steps = [i + 2 * forward_dir, j]
+        steps << two_steps if at_start_row? && board.empty?(two_steps)
+        steps
     end
 
     def side_attacks
+        i,j = pos
 
+        left_attack = [i + forward_dir, j - 1]
+        right_attack = [i + forward_dir, j + 1]
+        side_moves = [left_attack,right_attack]
+
+        side_moves.select do |move|
+            if board.empty?(move) || !board.valid_moves(move)
+                return false
+            else
+                attacked_piece = board[move]    
+                attacked_piece && attacked_piece.color != color
+            end
+        end
     end
 end
---
+++ Pieces/Piece.rb
@@ -0,0 +1,35 @@
+class Piece
+
+    attr_reader :color, :board
+    attr_accessor :pos 
+
+    def initialize(color,board,pos)
+        @color,@board,@pos = color,board,pos
+    end
+
+    def to_s
+        " #{symbol} "
+    end
+
+    def empty?
+        false
+    end
+
+    def valid_moves(pos)
+
+    end
+
+
+    def symbol
+
+    end
+
+    private
+    def move_into_check?(end_pos)
+
+    end
+end
+
+
+
+
